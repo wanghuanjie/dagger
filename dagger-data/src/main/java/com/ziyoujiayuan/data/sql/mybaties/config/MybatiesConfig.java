618ef2b279ff767405853e03647d42f55714707a
王焕杰 <huanjie.wang@lanmaoly.com>
2017-12-19 13:33:06 +0800

    升级tomcat-jdbc数据库连接池为c3p0

--- dagger-data/src/main/java/com/ziyoujiayuan/data/sql/mybaties/config/MybatiesConfig.java
+++ dagger-data/src/main/java/com/ziyoujiayuan/data/sql/mybaties/config/MybatiesConfig.java
@@ -1,16 +1,21 @@
 package com.ziyoujiayuan.data.sql.mybaties.config;
 
+import javax.sql.DataSource;
+
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.tomcat.jdbc.pool.DataSource;
+//import org.apache.tomcat.jdbc.pool.DataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 /**
  * Mybaties相关配置
@@ -21,11 +26,20 @@ import org.springframework.transaction.PlatformTransactionManager;
 @MapperScan("com.ziyoujiayuan.data.sql.mybaties.mapper")
 public class MybatiesConfig {
 
-	@Bean
-	@ConfigurationProperties(prefix="spring.datasource")
-	public DataSource dataSource() {
-		return new DataSource();
-	}
+	
+//	@Bean
+//	@ConfigurationProperties(prefix="spring.datasource")
+//	public DataSource dataSource() {
+//		DataSource dataSource = new DataSource();
+//		dataSource.setTestWhileIdle(true);
+//		return  dataSource;
+//	}
+	
+    @Bean
+    @ConfigurationProperties(prefix="c3p0")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().type(ComboPooledDataSource.class).build();
+    }
 	
 	@Bean
 	public SqlSessionFactory sqlSessionFactoryBean() throws Exception{
